601a275040148d0427c6042a3fcccc3b3cf4e921
justdoitjun <bing74700@gmail.com>
2023-09-23 13:23:25 +0900

    justdoitjun- webSocket(업비트) 에러 - 09232023

--- src/main/java/com/kbstar/controller/StockController.java
+++ src/main/java/com/kbstar/controller/StockController.java
@@ -1,27 +1,33 @@
 package com.kbstar.controller;
 
-import com.kbstar.dto.RandomChatRoom;
-import com.kbstar.service.randomChat.RandomChatServiceMain;
+
+import com.kbstar.util.StockPriceAPI;
+import com.kbstar.util.StockSearchAPI;
 import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 @RequestMapping("/stock")
 @Slf4j
 public class StockController {
 
+    @Autowired
+    StockPriceAPI stockPriceAPI;
+    @Autowired
+    StockSearchAPI stockSearchAPI;
+
+
     String dir ="stock/";
-
-
-
-    // 스프링 시큐리티의 로그인 유저 정보는 Security 세션의 PrincipalDetails 안에 담긴다
-    // 정확히는 PrincipalDetails 안에 ChatUser 객체가 담기고, 이것을 가져오면 된다.
 
 
 
@@ -30,7 +36,25 @@ public class StockController {
         model.addAttribute("center", dir + "overlook");
         return "index";
     }
+    @GetMapping("/price")
+    @ResponseBody
+    public String stockPriceApi(@RequestParam("function") String function, @RequestParam("stockCodes") String stockCodes) throws Exception {
+        JSONObject result = new JSONObject();
+        result.put("stockPrice", stockPriceAPI.generateCode(function, stockCodes));
 
+        return result.toJSONString();
+    }
+
+    @RequestMapping("/codes")
+    @ResponseBody
+    public ResponseEntity<List<?>> stockSearchApi(@RequestParam("keywords") String keywords) throws Exception {
+
+        JSONArray result = new JSONArray();
+        String stockCodes = stockSearchAPI.generateCode(keywords);
+        result.add(stockCodes);
+
+        return ResponseEntity.ok(result);
+    }
 
 
 }
---
+++ src/main/java/com/kbstar/util/StockSearchAPI.java
@@ -0,0 +1,62 @@
+package com.kbstar.util;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONObject;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@Slf4j
+@Component("StockSearchAPI")
+public class StockSearchAPI {
+    private static final String function = "SYMBOL_SEARCH";
+    private static final String authkey = "L646A87J103HCPR7";
+    private static final String BASE_URL = "https://www.alphavantage.co/query";
+    private final HttpClient client;
+
+    public StockSearchAPI() {
+        client = HttpClient.newBuilder().build();
+    }
+    //https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=AUTHKEY1234567890&searchdate=20180102&data=AP01
+    public String generateCode(String keywords) throws Exception {
+        String code = "";
+
+        // JSON 형식의 요청 본문 생성
+        JsonObject requestBody = new JsonObject();
+
+        // Gson을 사용하여 JSON 요청 본문을 문자열로 변환
+        Gson gson = new Gson();
+        String requestBodyString = gson.toJson(requestBody);
+
+        JSONObject obj;
+        obj = null;
+//        https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&apikey=demo
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(BASE_URL + "?function=" + function + "&keywords=" + keywords + "&apikey="+authkey))
+                    .GET()
+                    .build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() == 200) {
+                code = response.body();
+
+                log.info("==========");
+                log.info(code);
+                log.info("==========");
+            } else {
+                System.out.println("API request failed. Response code: " + response.statusCode());
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        return code;
+    }
+}
