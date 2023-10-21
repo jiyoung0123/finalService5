4efaef91893bd806c5657c4415684c49e7b74c49
justdoitjun <bing74700@gmail.com>
2023-10-21 11:30:23 +0900

    [최준혁]

---
+++ src/main/java/com/kbstar/controller/BalanceController.java
@@ -0,0 +1,74 @@
+package com.kbstar.controller;
+
+
+import com.kbstar.util.StockPriceAPI;
+import com.kbstar.util.StockSearchAPI;
+import com.kbstar.util.WebCrawler;
+import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+@Controller
+@RequestMapping("/stock")
+@Slf4j
+public class StockController {
+
+    @Autowired
+    StockPriceAPI stockPriceAPI;
+    @Autowired
+    StockSearchAPI stockSearchAPI;
+
+    @Autowired
+    WebCrawler webCrawler;
+
+
+    String dir ="stock/";
+
+
+
+    @RequestMapping("")
+    public String main(Model model, HttpSession session) throws Exception {
+        model.addAttribute("center", dir + "overlook");
+        return "index";
+    }
+    @GetMapping("/price")
+    @ResponseBody
+    public String stockPriceApi(@RequestParam("function") String function, @RequestParam("stockCodes") String stockCodes) throws Exception {
+        JSONObject result = new JSONObject();
+        result.put("stockPrice", stockPriceAPI.generateCode(function, stockCodes));
+
+        return result.toJSONString();
+    }
+
+    @RequestMapping("/codes")
+    @ResponseBody
+    public ResponseEntity<List<?>> stockSearchApi(@RequestParam("keywords") String keywords) throws Exception {
+
+        JSONArray result = new JSONArray();
+        String stockCodes = stockSearchAPI.generateCode(keywords);
+        result.add(stockCodes);
+
+        return ResponseEntity.ok(result);
+    }
+
+    @RequestMapping("/crawl")
+    @ResponseBody
+    public Object crwalStock() throws Exception {
+
+        JSONArray result = new JSONArray();
+        Object crawledResult = webCrawler.generateCode();
+
+        return crawledResult;
+    }
+
+
+}
---
+++ src/main/java/com/kbstar/util/AlphaPriceAPI.java
@@ -0,0 +1,68 @@
+package com.kbstar.util;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import lombok.Builder;
+import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONObject;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Component("StockPriceAPI")
+public class StockPriceAPI {
+    //private static final String function = "TIME_SERIES_DAILY_ADJUSTED";
+    private static final String authkey = "L646A87J103HCPR7";
+    private static final String BASE_URL = "https://www.alphavantage.co/query";
+    private final HttpClient client;
+
+    public StockPriceAPI() {
+        client = HttpClient.newBuilder().build();
+    }
+    //https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=AUTHKEY1234567890&searchdate=20180102&data=AP01
+    public String generateCode(String function, String symbol) throws Exception {
+        String code = "";
+        //List<String> list = new ArrayList<>();
+
+        // JSON 형식의 요청 본문 생성
+        JsonObject requestBody = new JsonObject();
+
+        // Gson을 사용하여 JSON 요청 본문을 문자열로 변환
+        Gson gson = new Gson();
+        String requestBodyString = gson.toJson(requestBody);
+
+        JSONObject obj;
+        obj = null;
+//        https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&apikey=demo
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(BASE_URL + "?function=" + function + "&symbol=" + symbol + "&apikey="+authkey))
+                    .GET()
+                    .build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() == 200) {
+                code = response.body();
+                //list.add(response.body());
+
+                log.info("==========");
+                log.info(code.toString());
+                log.info("==========");
+            } else {
+                log.debug("API request failed. Response code: " + response.statusCode());
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        return code;
+    }
+}
